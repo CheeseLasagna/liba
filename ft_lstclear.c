aff70f32bdb5c16587b74027c8d08965e28ecfc1
rustam <rustam-aliev@hotmail.com>
2020-05-26 15:57:57 +0300

    final4

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: tlavelle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 11:20:51 by tlavelle          #+#    #+#             */
-/*   Updated: 2020/05/26 15:38:00 by tlavelle         ###   ########.fr       */
+/*   Updated: 2020/05/26 15:56:44 by tlavelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,14 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	t_list *temp;
 	
 	if (!lst)
-		return (NULL);
+		return ;
 	current = *lst;
 	while (current != NULL)
 	{
 		temp = current;
 		del(current->content);
 		current = current->next;
-		free(temp);	
+		free(temp);
+		temp = NULL;	
 	}
-	free(*lst);
-	*lst = NULL;
 }
